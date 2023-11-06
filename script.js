5359eefe2a71cdda99dbc425ad781d4606d38a7a
AmalAlshehrig <amalalshehrig@gmail.com>
2023-11-06 15:36:16 +0300

    add

---
+++ script.js
@@ -0,0 +1,37 @@
+let TVscreen=document.getElementById("screen1")
+let one=document.getElementById("one")
+let two=document.getElementById("two")
+let three=document.getElementById("three")
+let four=document.getElementById("four")
+let five=document.getElementById("five")
+let six=document.getElementById("six")
+let seven=document.getElementById("seven")
+let eight=document.getElementById("eight")
+let nine=document.getElementById("nine")
+one.addEventListener("click", function clickone(){
+TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+})
+two.addEventListener("click", function clicktwo(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+})
+three.addEventListener("click", function clickthree(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+})
+four.addEventListener("click", function clickfour(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+})
+five.addEventListener("click", function clickfive(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+})
+six.addEventListener("click", function clicksix(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+})
+seven.addEventListener("click", function clickseven(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+})
+eight.addEventListener("click", function clickeight(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+})
+nine.addEventListener("click", function clicknine(){
+    TVscreen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+})
